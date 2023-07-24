6e060bb23343a506803a36f4d3669a08d4bb1d98
Zach Buth <zachbuth@gmail.com>
2023-07-24 17:02:59 -0400

    1st

---
+++ Movie Database/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Movie_Database
+{
+	public class Movie
+	{
+		//properties
+		public string Title { get; set; }
+		public string Category { get; set; }
+
+
+		//constructor
+		public Movie(string _title, string _category)
+		{
+			Title = _title;
+			Category = _category;
+		}
+
+		//method
+		
+	}
+}
+
---
+++ Movie Database/Program.cs
@@ -0,0 +1,32 @@
+﻿using Movie_Database;
+using Circle_Lab;
+
+Console.WriteLine("Hello, Welcome to the Movie Database!");
+
+List<Movie> listMovie = new List<Movie>
+{
+    new Movie("A Bug's Life", "Animated"),
+    new Movie("There Will Be Blood", "Drama"),
+    new Movie("Interstellar", "Sci-Fi"),
+    new Movie("The Ring", "Animated"),
+    new Movie("Lincoln", "Drama"),
+    new Movie("The Hills Have Eyes", "Horror"),
+    new Movie("Oppenheimer", "Drama"),
+    new Movie("Finding Nemo", "Animated"),
+    new Movie("Alien", "Sci-fi"),
+    new Movie("Final Destination", "Horror"),
+};
+
+bool runProgram = true;
+while (runProgram)
+{
+
+    Console.WriteLine("Enter a category");
+    string input = Console.ReadLine().ToLower().Trim();
+    List<Movie> Movie = listMovie.Where(m => m.Category.ToLower() == input).ToList();
+    foreach (Movie m in Movie)
+    {
+        Console.WriteLine(m.Title);
+    }
+    runProgram = Validator.GetContinue();
+}
